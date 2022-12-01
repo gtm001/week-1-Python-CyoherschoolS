19e698606a0321fe7ed83e026b68351a4a7b51b4
gtm001 <119605352+gtm001@users.noreply.github.com>
2022-12-01 23:37:05 +0530

    Update Conditional Statements - if-elif-else  Class 6-Cypherschools.py

---
+++ Conditional Statements - if-elif-else  Class 6-Cypherschools.py
@@ -0,0 +1,11 @@
+use of if, else, elif statements
+age=int(input("enter your age: "))
+if age>=18:
+    print("you are adult")
+    print("you can drive")
+elif age<=3:
+    print("you are child")  
+    print("go to school ")  
+else:
+    print("you are kid")
+    print("be in home!")
